3a0bc8f1691e88f4964d5af25b6b168284d8b1bf
xenakash <146567083+xenakash@users.noreply.github.com>
2025-05-23 03:29:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,116 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_mysqldb import MySQL
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_socketio import SocketIO, join_room, leave_room, emit
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+# MySQL config
+app.config['MYSQL_HOST'] = os.environ.get('sql12.freesqldatabase.com')
+app.config['MYSQL_USER'] = os.environ.get('sql12780618')
+app.config['MYSQL_PASSWORD'] = os.environ.get('gW4ryzi13A')
+app.config['MYSQL_DB'] = os.environ.get('sql12780618')
+mysql = MySQL(app)
+
+# Login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# SocketIO
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# User class
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+@login_manager.user_loader
+def load_user(user_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE id=%s", (user_id,))
+    user = cur.fetchone()
+    if user:
+        return User(int(user[0]), user[1], user[2])
+    return None
+
+# Forms
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6)])
+    confirm = PasswordField('Confirm Password', validators=[EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
+
+# Routes
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_pw = generate_password_hash(form.password.data)
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (form.username.data, hashed_pw))
+        mysql.connection.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username=%s", (form.username.data,))
+        user = cur.fetchone()
+        if user and check_password_hash(user[2], form.password.data):
+            login_user(User(user[0], user[1], user[2]))
+            return redirect(url_for('chat'))
+        flash('Invalid credentials')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/chat')
+@login_required
+def chat():
+    return render_template('chat.html', username=current_user.username)
+
+# SocketIO event handlers
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    emit('message', {'user': 'System', 'msg': f'{username} has joined the room.'}, room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    emit('message', {'user': 'System', 'msg': f'{username} has left the room.'}, room=room)
+
+@socketio.on('send_message')
+def handle_message(data):
+    room = data['room']
+    msg = data['msg']
+    username = data['username']
+    emit('message', {'user': username, 'msg': msg}, room=room)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
